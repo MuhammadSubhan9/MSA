93eacc00640994345af6dbcff3b3ec4311d62977
Muhammad Subhan <128174642+MuhammadSubhan9@users.noreply.github.com>
2026-07-25 23:49:37 +0300

    Resolve Git merge conflicts

--- js/skills.js
+++ js/skills.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded",()=>{
 
 
@@ -48,54 +47,3 @@ document.addEventListener("DOMContentLoaded",()=>{
     
     
     });
-=======
-document.addEventListener("DOMContentLoaded",()=>{
-
-
-    const fills =
-    document.querySelectorAll(".fill");
-    
-    
-    fills.forEach(fill=>{
-    
-        fill.style.setProperty(
-            "--width",
-            fill.dataset.width
-        );
-    
-    
-    });
-    
-    
-    const observer =
-    new IntersectionObserver(entries=>{
-    
-    
-    entries.forEach(entry=>{
-    
-    
-    if(entry.isIntersecting){
-    
-    
-    entry.target.classList.add("active");
-    
-    
-    }
-    
-    
-    });
-    
-    
-    },{threshold:.5});
-    
-    
-    
-    fills.forEach(fill=>{
-    
-    observer.observe(fill);
-    
-    });
-    
-    
-    });
->>>>>>> eb69bdb4dda774a4d4c353d302a6becab1a27222
